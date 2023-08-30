559918e49bce0af3037aad64d9c6289836a06a23
Airuiya <gmt466203372@163.com>
2023-08-30 14:10:59 +0800

    首次上传webpack_code

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    // 入口
+    entry: "",
+    // 输出
+    output: {},
+    // 加载器
+    module: {
+      rules: [],
+    },
+    // 插件
+    plugins: [],
+    // 模式
+    mode: "",
+  };
+  
